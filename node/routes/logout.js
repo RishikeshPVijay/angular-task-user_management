9769bd5ac2a2971f41c43685d16768a34478625d
Rishikesh P Vijay <rishikeshpvijay@gmail.com>
2020-12-13 19:50:59 +0530

    first

--- node/routes/logout.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-
-router.get('/logout', (req, res) => {
-    req.logout();
-    res.clearCookie('user_sid');
-    res.json({logout: true});
-});
-
-module.exports = router;
